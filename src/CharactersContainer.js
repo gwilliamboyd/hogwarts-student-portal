dcdc0106076ff10d6876e0b104320ab9e47cbc66
gwilliamboyd <gwillboyd@outlook.com>
2023-01-01 13:14:09 -0600

    starts characters page, creates/styles title/input field

--- src/CharactersContainer.js
+++ src/CharactersContainer.js
@@ -1,8 +1,17 @@
 const CharactersContainer = () => {
   return (
     <>
-        <h1>Characters</h1>
-        <p>Characters go here!</p>
+      <div className="characters-master">
+        <div className="characters-heading">
+          <div className="characters-title">
+            Type any character's name or info here
+          </div>
+          <div className="characters-promise">
+            <i>(But first, you must solemnly swear you are <b>not</b> up to no good...)</i>
+          </div>
+          <input type="text" className="characters-search-input" placeholder="Make sure you're not using invisible ink!" />
+        </div>
+      </div>
     </>
   )
 }
