27282f54e8ca02040373debc03d45e9d27f46fd1
gwilliamboyd <gwillboyd@outlook.com>
2023-01-19 13:44:37 -0600

    Spells page scrolls properly

--- src/SpellsBook.js
+++ src/SpellsBook.js
@@ -1,41 +1,45 @@
-import { useState, useEffect } from "react"
+import { useEffect, useState } from "react"
 import SpellsEntry from "./SpellsEntry"
 
-const SpellsBook = ({ page, setPage, spells }) => {
-    const currentSpells = writeSpellPages()
-    
-    console.log(spells)
-    console.log(currentSpells)
-    // Creates first 2 pages
-    function writeSpellPages(currentSpells) {
-        currentSpells = [spells[page], spells[page + 1]]
-        return currentSpells
-    }    
+// Creates first 2 pages
+function writeSpellPages(spellBooks, page) {
+    let currentSpells = [spellBooks[page], spellBooks[page + 1]]
+    return currentSpells
+} 
+
+const SpellsBook = ({ page, setPage, spellBooks }) => {
+
+    const [currentSpells, setCurrentSpells] = useState(writeSpellPages(spellBooks, page))
+
     // Runs paginate on load
+
     useEffect(() => {
-        console.log('page loaded')
-        writeSpellPages(currentSpells)
-    }, [])
+        setCurrentSpells(writeSpellPages(spellBooks, page))
+    }, [spellBooks, page])
+
+
     // Flips to next 2 pages
     function nextPage() {
+        // Catch error at end of spells
+        // setCurrentSpells(writeSpellPages(spellBooks, (page + 2)))
         setPage(page + 2)
-        writeSpellPages()
+        // writeSpellPages(spellBooks, page)
     }
 
-    // // Right arrow key functionality
+    // Right arrow key functionality
+    // Insert into useEffect:
     window.addEventListener('keydown', (e) => {
         if(e.keyCode === 39) {
-            // console.log('right')
             nextPage()
         }
     })
-     
+
   return (
     <>
         <div className="spells-book">
             <div className="spells-page-one">
                 <ul>
-                {currentSpells[0].map((s) => {
+                {currentSpells?.[0]?.map((s) => {
                         return (
                             <SpellsEntry 
                             id={s.id}
@@ -47,7 +51,7 @@ const SpellsBook = ({ page, setPage, spells }) => {
             </div>
             <div className="spells-page-two">
                 <ul>
-                    {currentSpells[1].map((s) => {
+                    {currentSpells?.[1]?.map((s) => {
                         return (
                             <SpellsEntry 
                             id={s.id}
--- src/SpellsContainer.js
+++ src/SpellsContainer.js
@@ -3,91 +3,50 @@ import axios from "axios"
 import SpellsBook from "./SpellsBook"
 
 const SpellsContainer = () => {
-    // Variables
-    const spellsUrl = 'https://hp-api.onrender.com/api/spells'
+
     const spellsPerPage = 4
+
     // State initialization
     const [loading, setLoading] = useState(false)
     const [spells, setSpells] = useState([])
     const [page, setPage] = useState(0)
-    const [currentSpells, setCurrentSpells] = useState([])
+
     // Gets spells from API
     useEffect(() => {
         const getSpells = async () => {
             setLoading(true)
-            const res = await axios.get(spellsUrl)
+            const res = await axios.get('https://hp-api.onrender.com/api/spells')
             setSpells(res.data)
             setLoading(false)
         }
         getSpells()
     }, [])
-    console.log(spells)
+
     // Split up spells array into chunks of 4
     function paginate() {
         const result = spells.reduce((spellPage, item, index) => { 
             const chunkIndex = Math.floor(index/spellsPerPage)        
             if(!spellPage[chunkIndex]) {
-            spellPage[chunkIndex] = [] // start a new chunk
+                spellPage[chunkIndex] = [] // start a new chunk
             }        
             spellPage[chunkIndex].push(item)
-            return spellPage
-    
+            return spellPage    
         }, [])
         return result
     }
-
+    
     const spellBooks = paginate()
-
-/*     const currentSpells = writeSpellPages()
-    // Creates first 2 pages
-    function writeSpellPages(currentSpells) {
-        currentSpells = [spellBooks[page], spellBooks[page + 1]]
-        // console.log(currentSpells)
-        return currentSpells
-    }
-    // console.log(currentSpells)
-    // Runs paginate on load
-    useEffect(() => {
-        writeSpellPages(currentSpells)
-    }, [])
-
-    // Flips to next 2 pages
-    function nextPage() {
-        setPage(page + 2)
-        writeSpellPages()
-    } */
-
-/*     function pageSpells(totalPages) {
-        spells.forEach()
-    }
-
-    let totalPages = []
-    
-
-    function paginate() {
-        for(let i = 1; i <= Math.ceil(spells.length / spellsPerPage); i++) {
-            totalPages.push(i)
-        }        
-        console.log(totalPages)
-        return totalPages
-    }
-    // Create proper number of pages on load
-    
-    console.log(currentSpells) 
-*/
 
     return (
         <>
             <div className="spells-master">
                 <div className="spells-container">
-                    <h1 className="spells-header"/*  onClick={nextPage} */>
+                    <h1 className="spells-header">
                         Spells Reference - 1992 Edition
                     </h1>
                     <SpellsBook page={page} 
                     setPage={setPage} 
-                    spells={spellBooks}
-                    currentSpells={currentSpells}
-                    setCurrentSpells={setCurrentSpells} />
+                    spellBooks={spellBooks} />
                 </div>
             </div>
         </>
