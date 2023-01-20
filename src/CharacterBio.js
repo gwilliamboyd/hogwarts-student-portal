ad2028d6f882d220d46ecd3e103864691e4598af
gwilliamboyd <gwillboyd@outlook.com>
2023-01-20 13:44:13 -0600

    Characters restyle 90%
    Characters are almost restyled, at a stopping point

--- src/CharacterBio.js
+++ src/CharacterBio.js
@@ -2,36 +2,36 @@ const CharacterBio = ({ image, name, house, patronus, id }) => {
 
   return (
     <li className="characters-bio">
-        <table className="characters-table">
-            <tbody>
-                <tr className="characters-table-row">
-                    <td className="characters-table-cell characters-image">
+        <div className="characters-table">
+            <div>
+                <div className="characters-table-row">
+                    <div className="characters-table-cell characters-image">
                         <img src={image} />
-                    </td>
-                    <td className="characters-table-cell characters-name">
-                        <span className="characters-name-heading">
-                            Name:
-                        </span>
-                        <br />
-                        {name}
-                    </td>
-                    <td className="characters-table-cell characters-house">
-                        <span className="characters-house-heading">
-                            House:    
-                        </span>
-                        <br />   
-                        {house}
-                    </td>
-                    <td className="characters-table-cell characters-patronus">
-                        <span className="characters-patronus-heading">
-                            Patronus:    
-                        </span>
-                        <br />   
-                        {patronus}
-                    </td>
-                </tr>
-            </tbody>
-        </table>        
+                    </div>
+                    <div className="characters-text">
+                        <div className="characters-table-cell characters-name">
+                            <span className="characters-name-heading">
+                                Name:
+                            </span>
+                            {name}
+                        </div>
+                        <div className="characters-table-cell characters-house">
+                            <span className="characters-house-heading">
+                                House:    
+                            </span>  
+                            {house}
+                        </div>
+                        <div className="characters-table-cell characters-patronus">
+                            <span className="characters-patronus-heading">
+                                Patronus:    
+                            </span>  
+                            {patronus}
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>        
     </li>
   )
 }
